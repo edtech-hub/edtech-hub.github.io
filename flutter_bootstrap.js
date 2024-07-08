6d3f24fe2a2bd648f51e6bfd056cc8c82dc3aa3a
Biggi <admin@spookysoul.com>
2024-07-08 14:36:54 +0530

    1.0.004

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b4bfd459865a8d636f26aca0d330ae297c006c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1162040782"
+    serviceWorkerVersion: "3455686864"
   }
 });
